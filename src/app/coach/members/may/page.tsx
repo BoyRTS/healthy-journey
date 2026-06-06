eae66319df860354a16604a53555476874f0371b
Boyd <boyrts@gmail.com>
2026-06-06 15:40:16 +0700

    Remove hardcoded coach member routes

--- src/app/coach/members/may/page.tsx
+++
@@ -1,6 +0,0 @@
-import { MemberDetailPage } from "@/components/coach/MemberDetailPage";
-import { mayMemberDetail } from "@/data/mockCoachMemberDetails";
-
-export default function MayMemberDetailPage() {
-  return <MemberDetailPage data={mayMemberDetail} />;
-}
--- src/app/coach/members/prae/page.tsx
+++
@@ -1,6 +0,0 @@
-import { MemberDetailPage } from "@/components/coach/MemberDetailPage";
-import { praeMemberDetail } from "@/data/mockCoachMemberDetails";
-
-export default function PraeMemberDetailPage() {
-  return <MemberDetailPage data={praeMemberDetail} />;
-}
